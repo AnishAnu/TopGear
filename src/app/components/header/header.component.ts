4d3f6e3ad39f3a3a6aa1b50fcb4cf93226194dc9
sushma <sushma.p65@wipro.com>
2018-05-09 16:49:56 +0530

    mobile nav side bar added

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  public isMobileNavVisible = false;
   constructor() { }
 
   ngOnInit() {
   }
 
+  public mobileNav(event) {
+		this.isMobileNavVisible = !this.isMobileNavVisible;
+		event.stopPropagation();
+		// if(this.isMobileNavVisible) {
+		// 	this.mobileNaveClass = 'mobile-left-nav-show';
+		// }
+		console.log('nav:-', this.isMobileNavVisible);
+	}
 }
